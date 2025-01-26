a2c99d3e1a66d73a22a05ebab4e3f574da284fb2
Esha Awasthi <eshaawasthi55@gmail.com>
2025-01-27 01:48:29 +0530

    Added avatar in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import { LOGO } from "../utils/constants";
+import { LOGO, USER_AVATAR } from "../utils/constants";
 
 const Header = () => {
   return (
     <div className="row header-wrapper position-absolute">
       <img src={LOGO} alt="logo" />
+      <img src={USER_AVATAR} alt="avatar" className="width-6 p-4" />
     </div>
   );
 };
